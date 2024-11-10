1680181f6ada93a8bf81b8a16746af5c7e0eff5d
jcserv <jarrod.servilla@gmail.com>
2024-11-09 19:37:07 -0500

    tweak: info sizing

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -11,13 +11,15 @@ function Index() {
   return (
     <section className="p-2 md:p-4">
       <div className="min-h-[80vh] w-full flex flex-col items-center">
-        <Alert className="w-full max-w-4xl bg-blue-400 dark:bg-blue-600 my-4 md:my-8">
+        <Alert className="w-1/2 max-w-4xl bg-blue-400 dark:bg-blue-600 my-4 md:my-8">
           <Info className="h-4 w-4" />
           <AlertTitle>Info</AlertTitle>
           <AlertDescription className="space-y-1">
-            <p>Enter the cards you want to bling out, and we&apos;ll show you</p>
-            <p>all of the most expensive printings available for each card, as well as</p>
-            <p>the total cost.</p>
+            <p>
+              Enter the cards you want to bling out, and we&apos;ll show you all
+              of the most expensive printings available for each card, as well
+              as the total cost.
+            </p>
           </AlertDescription>
         </Alert>
         <CartForm />
