4cdcc21b5288cd03de8c4dd1e0599effcea50a24
jcserv <jarrod.servilla@gmail.com>
2024-11-10 23:17:29 -0500

    fix: bug where changing printing would cause it not to be in clipboard

--- src/lib/decklist.ts
+++ src/lib/decklist.ts
@@ -2,7 +2,6 @@ import { CardOption, Treatment } from "@/types";
 
 export function toMoxfield(deck: CardOption[]): string {
   const selectedCards = deck
-    .filter((card) => card.selected)
     .map(
       (card) =>
         `${card.quantity} ${card.cardName} (${card.setCode.toUpperCase()}) ${card.collectorNumber}${card.selectedTreatment === Treatment.Foil ? " *f*" : ""}`,
